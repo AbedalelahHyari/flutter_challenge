9a7ae0076e3c84df08b8881ff7d1837115a264d0
AbedalelahHyari <abedalelah.hyari@gmail.com>
2022-03-22 22:35:49 +0300

    Write the insertion algorithm explnation

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 //////////////////////////////////////////////////////////////////////////////////////////
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
     return _MyAppState();
@@ -34,25 +35,27 @@ class _MyAppState extends State<MyApp> {
   }
 
 //////////////////////////////////////////////////////////////////////////////////////////
-/* 1- BubbleSort */
+
+// 1- BubbleSort //
 
 /*
  * Algorithm:-
  *
- * 1- we need to make a loop over the arr
+ * 1- I need to give value to the newArray which it a list of integers from the input field by using this built-in method
+ * json.decode(source)
  *
- * 2- then I need to give value to the newArray which it a list of integers from the input field
- *
- * 3-  I will make an instance from the Stopwatch to start counting the time execution for the method
+ * 2-  I will make an instance from the Stopwatch to start counting the time execution for the method
  *  to know the sorting time will take it.
  *
- * 4- we need to make another loop (nested) over the arr starting from the index 1 j = 1
+ * 3- we need to make a loop over the arr starting from the index (0) i = 0
+ *
+ * 4- we need to make a loop (nested) over the arr starting from the index (1) j = 1
  *
  * 5- I need to declare a boolean variable called swapped and give it false value
  *
- * 3- we need to make a comparison between two values
+ * 6- we need to make a comparison between two values
  *
- * 4- the condition will be if the second element smaller than the first one we need to make some steps:-
+ * 7- the condition will be if the second element smaller than the first one we need to make some steps:-
  *
  *      a- we need to save the smaller value in a temporary variable
  *
@@ -64,22 +67,20 @@ class _MyAppState extends State<MyApp> {
  *
  *      e- I will reassign the value for the variable swapped because we make an swap process between two value in the list
  *
- * 5- This process will coming over and over again even the array will be sorted in ascending order.
  *
- * 6- I will check if the swapped process not happened i will break the function
+ * 8- This process will coming over and over again even the array will be sorted in ascending order.
  *
- * 7- I will declare a variable called stop with value contain the time execution which i take it from
+ * 8- I will check if the swapped process not happened i will break the function
+ *
+ * 9- I will declare a variable called stop with value contain the time execution which i take it from
  * from the instance i declare it at the first of function (stopwatch1)
  *
- * 8- after this I will create setState method that will assign the value for the stop variable to the bubble variable.
+ * 10- after this I will create setState method that will assign the value for the (stop variable) to the (bubble variable).
  *  Then I will show the result inside the container
- *
- *
- *
  */
   void bubbleSort(arr) {
     newArray = List<int>.from(json.decode(arr));
-    print('undOrder bubble $newArray');
+    //print('undOrder bubble $newArray');
     Stopwatch stopwatch1 = Stopwatch()..start();
     for (int i = 0; i < newArray.length; i++) {
       bool swapped = false;
@@ -98,25 +99,50 @@ class _MyAppState extends State<MyApp> {
     setState(() {
       bubble = stop;
     });
-    print('Order bubble $newArray');
+    //print('Order bubble $newArray');
   }
 
   //////////////////////////////////////////////////////////////////////////////////////////
   /* InsertionSort */
+
+  /* Algorithm:-
+  *
+  * 1- at the first, I need to give value to the newArray which it a list of integers from the input field by using this built-in  * method json.decode(source)
+  *
+  * 2- I will make an instance from the Stopwatch to start counting the time execution for the method
+  *  to know the sorting time will take it.
+  *
+  * 3- I need to make a loop over the arr starting from index 1
+  *
+  * 4- I need to declare two variables currentElement and j and assign the currentElement to the current element in the array,     * assign the index to the previous element to the j
+  *
+  * 5- make a while loop that will check if the j more than or equal zero and newArray[j] > currentElement
+  *
+  * 6- if the condition was true make a shift between the two element
+  *
+  * 7- decrease the value for the j variable
+  *
+  * 8- outside the while loop i will make a swap newArray[j + 1] = currentElement;
+  *
+  * 9- I will declare a variable called stop with value contain the time execution which i take it from
+ * from the instance i declare it at the first of function (stopwatch2)
+ *
+ * 10- after this I will create setState method that will assign the value for the (stop variable) to the (bubble variable).
+ *  Then I will show the result inside the container
+ */
+
   void insertionSort(arr) {
     newArray = List<int>.from(json.decode(arr));
     print('undOrder insertion $newArray');
     Stopwatch stopwatch2 = Stopwatch()..start();
-    for (int i = 0; i < newArray.length; i++) {
-      var x = newArray[i], j = i;
-
-      while (j > 0 && newArray[j - 1] > x) {
-        newArray[j] = newArray[j - 1];
-        newArray[j - 1] = x;
+    for (int i = 1; i < newArray.length; i++) {
+      int currentElement = newArray[i];
+      int j = i - 1;
+      while (j >= 0 && newArray[j] > currentElement) {
+        newArray[j + 1] = newArray[j];
         j--;
       }
-
-      newArray[j] = x;
+      newArray[j + 1] = currentElement;
     }
     int stop = stopwatch2.elapsedMicroseconds;
     setState(() {
@@ -131,23 +157,25 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(title: Text("Sorting Algorithms Types")),
+            appBar: AppBar(title: const Text("Sorting Algorithms Types")),
             body: Column(
               children: <Widget>[
                 Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 50),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 50),
                   child: TextField(
                     onChanged: (text) {
-                      this.arr = text;
+                      arr = text;
                     },
-                    decoration: InputDecoration(
+                    decoration: const InputDecoration(
                       border: OutlineInputBorder(),
                       hintText: 'Enter The List here...',
                     ),
                   ),
                 ),
                 Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 100, vertical: 30),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 100, vertical: 30),
                   child: ElevatedButton(
                     child: const Text('Analyze Now'),
                     onPressed: () => {
@@ -157,23 +185,23 @@ class _MyAppState extends State<MyApp> {
                     },
                   ),
                 ),
-                Container(
+                SizedBox(
                   width: 300,
                   height: 80,
                   child: Card(
                       child: ListTile(
-                    leading: FlutterLogo(size: 30.0),
+                    leading: const FlutterLogo(size: 30.0),
                     title: Text(
                       'Bubble Sort:\n' + bubble.toString() + '  us',
                     ),
                   )),
                 ),
-                Container(
+                SizedBox(
                   width: 300,
                   height: 80,
                   child: Card(
                       child: ListTile(
-                    leading: FlutterLogo(size: 30.0),
+                    leading: const FlutterLogo(size: 30.0),
                     title: Text(
                       'Insertion Sort:\n' + insertion.toString() + '  us',
                     ),
